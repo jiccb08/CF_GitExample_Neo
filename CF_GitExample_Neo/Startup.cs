3239e3bfdd20debacba2b78686659498c9c288fd
sszpunar <sszpunar@live.com>
2017-12-04 13:36:33 -0500

    Add project files.

---
+++ CF_GitExample_Neo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CF_GitExample_Neo.Startup))]
+namespace CF_GitExample_Neo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

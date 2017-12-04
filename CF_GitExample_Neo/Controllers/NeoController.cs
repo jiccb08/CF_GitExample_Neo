af64648ee704094415dd098a384f8cdd55f5db7f
sszpunar <sszpunar@live.com>
2017-12-04 13:58:36 -0500

    added-sean-resources
    This is my first publish as a developer. I'm including a new controller, a new view, and a new action result in the controller for the view.

---
+++ CF_GitExample_Neo/Controllers/NeoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CF_GitExample_Neo.Controllers
+{
+    public class NeoController : Controller
+    {
+        // GET: Neo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Neo
+        public ActionResult Sean()
+        {
+            return View();
+        }
+    }
+}
